f6f42244e491c1b6ece233c0dd21e8e86bcf4e90
eiisoj <jmdc0624@outlook.com>
2025-02-13 02:56:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "W, I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "I love you so much, MMMMMMMMMMMMMUAH",
+        message: "I love you so much, MMMMMUAH",
         emojis: "💋💋💋💋💋💋💋💋💋💋💋💋💋💋💋💋💋"  // These will bounce around
     },
 
